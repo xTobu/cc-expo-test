1e0812ebda6bd7cb37984a6c54c85e57e4a9aedc
xTobu <junxiang.oriint@gmail.com>
2023-04-14 17:26:59 +0800

    [update] 1.0.0

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Updates from "expo-updates";
+import Constants from "expo-constants";
 
 export default function App() {
+  const [isAvailable, setIsAvailable] = useState(false);
+
+  async function onFetchUpdateAsync() {
+    try {
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        setIsAvailable(update.isAvailable);
+      } catch (error) {
+        // alert(`Error fetching latest Expo update: ${error}`);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Version</Text>
-      <Text>INIT</Text>
+      <Text>Version: {Constants.expoConfig.version}</Text>
+      <Text>isAvailable: {isAvailable.toString()}</Text>
+
+      <Button title="Fetch update" onPress={onFetchUpdateAsync} />
       <StatusBar style="auto" />
     </View>
   );
